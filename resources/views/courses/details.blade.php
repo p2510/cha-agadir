92b96f206180052652370c7be518a0a146d380aa
YATACHI <60470788+p2510@users.noreply.github.com>
2023-05-19 21:22:32 +0200

    update

--- resources/views/courses/details.blade.php
+++ resources/views/courses/details.blade.php
@@ -194,8 +194,10 @@
                                         </li>
                                         <li><a href="{{ route('about.index') }}">{{ __('header.subtitle2') }}</a>
                                         </li>
+                                        <li><a href="{{ route('index.team') }}">{{ __('header.subtitle3') }}</a>
+                                        </li>
                                         <li><a
-                                                href="{{ route('responsable.index') }}">{{ __('header.subtitle3') }}</a>
+                                                href="{{ route('index.professor') }}">{{ __('header.subtitle33') }}</a>
                                         </li>
                                         <li><a href="{{ route('campus.index') }}">{{ __('header.subtitle4') }}</a>
                                         </li>
