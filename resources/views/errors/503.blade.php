19ffd9faee3d59cfe940dbe4622f7436be026f6d
YATACHI <60470788+p2510@users.noreply.github.com>
2023-06-26 12:56:00 +0200

    add down page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.static')
+@section ('title')
+En maintenance
+@endsection
+@section('content')
+  <!-- Start main-content -->
+  <div class="main-content">
+    <!-- Section: home -->
+    <section id="home" class="bg-lightest fullscreen">
+      <div class="display-table text-center">
+        <div class="display-table-cell">
+          <div class="container pt-0 pb-0"><div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                <h1 class="text-theme-colored2 font-weight-600 font-64 mb-0">Site en construction</h1>
+                <p class="font-14">Désolé.... Nous sommes en mode maintenance .<br>Nous reviendrons très bientôt....</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <!-- end main-content -->
+@endsection
