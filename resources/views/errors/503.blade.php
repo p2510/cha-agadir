94984674597906a75655c9a0cafcdc687902f04a
YATACHI <60470788+p2510@users.noreply.github.com>
2023-06-26 13:04:45 +0200

    update

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,24 +1,171 @@
-@extends('layouts.static')
-@section ('title')
-En maintenance
-@endsection
-@section('content')
-  <!-- Start main-content -->
-  <div class="main-content">
-    <!-- Section: home -->
-    <section id="home" class="bg-lightest fullscreen">
-      <div class="display-table text-center">
-        <div class="display-table-cell">
-          <div class="container pt-0 pb-0"><div class="row">
-              <div class="col-md-10 col-md-offset-1">
-                <h1 class="text-theme-colored2 font-weight-600 font-64 mb-0">Site en construction</h1>
-                <p class="font-14">Désolé.... Nous sommes en mode maintenance .<br>Nous reviendrons très bientôt....</p>
-              </div>
-            </div>
-          </div>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="Cha agadir " />
+
+    <meta name="keywords" content="education,school,university,educational,learn,learning,teaching,workshop" />
+    <meta name="author" content="ThemeMascot" />
+    <!-- Page Title -->
+    <title>Cha Agadir</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link href="{{ asset('images/logo.svg') }}" rel="shortcut icon" type="image/svg">
+    <link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+    <!-- Stylesheet -->
+    <link href="{{ asset('app-assets/css/bootstrap.min.css') }}" rel='stylesheet' type='text/css'>
+    <link href="{{ asset('app-assets/css/jquery-ui.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('app-assets/css/animate.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('app-assets/css/css-plugin-collections.css') }}" rel="stylesheet" />
+    <!-- CSS | menuzord megamenu skins -->
+    <link href="{{ asset('app-assets/css/menuzord-megamenu.css') }}" rel="stylesheet" />
+    <link id="menuzord-menu-skins" href="{{ asset('app-assets/css/menuzord-skins/menuzord-boxed.css') }}"
+        rel="stylesheet" />
+    <!-- CSS | Main style file -->
+    <link href="{{ asset('app-assets/css/style-main.css') }}" rel="stylesheet" type="text/css">
+    <!-- CSS | Preloader Styles -->
+    <link href="{{ asset('app-assets/css/preloader.css') }}" rel="stylesheet" type="text/css">
+    <!-- CSS | Custom Margin Padding Collection -->
+    <link href="{{ asset('app-assets/css/custom-bootstrap-margin-padding.css') }}" rel="stylesheet" type="text/css">
+    <!-- CSS | Responsive media queries -->
+    <link href="{{ asset('app-assets/css/responsive.css') }}" rel="stylesheet" type="text/css">
+    <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+    <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+    <!-- Revolution Slider 5.x CSS settings -->
+    <link href="{{ asset('app-assets/js/revolution-slider/css/settings.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('app-assets/js/revolution-slider/css/layers.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('app-assets/js/revolution-slider/css/navigation.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- CSS | Theme Color -->
+    <link href="{{ asset('app-assets/css/colors/theme-skin-color-set1.css') }}" rel="stylesheet" type="text/css">
+    <!-- external javascripts -->
+    <script src="{{ asset('app-assets/js/jquery-2.2.4.min.js') }}"></script>
+    <script src="{{ asset('app-assets/js/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('app-assets/js/bootstrap.min.js') }}"></script>
+    <!-- JS | jquery plugin collection for this theme -->
+    <script src="{{ asset('app-assets/js/jquery-plugin-collection.js') }}"></script>
+
+    <!-- Revolution Slider 5.x SCRIPTS -->
+    <script src="{{ asset('app-assets/js/revolution-slider/js/jquery.themepunch.tools.min.js') }}"></script>
+    <script src="{{ asset('app-assets/js/revolution-slider/js/jquery.themepunch.revolution.min.js') }}"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
+
+    
+
+    <style>
+        @keyframes blink {
+            0% {
+                opacity: 1;
+            }
+
+            50% {
+                opacity: 0.5;
+            }
+
+            100% {
+                opacity: 1;
+            }
+        }
+    </style>
+</head>
+
+
+
+<body class="">
+    <div id="wrapper" class="clearfix">
+        <!-- Start main-content -->
+        <div class="main-content">
+            <!-- Section: home -->
+            <section id="home" class="bg-lightest fullscreen">
+                <div class="display-table text-center">
+                    <div class="display-table-cell">
+                        <div class="container pt-0 pb-0">
+                            <div class="row">
+                                <div class="col-md-10 col-md-offset-1">
+                                    <h1 class="text-theme-colored2 font-weight-600 font-64 mb-0">Site en construction
+                                    </h1>
+                                    <p class="font-14">Désolé.... Nous sommes en mode maintenance .<br>Nous reviendrons
+                                        très bientôt....</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-      </div>
-    </section>
-  </div>
-  <!-- end main-content -->
-@endsection
+    </div>
+
+
+
+<!-- end main-content -->
+<style>
+  @media screen and (max-width: 768px) {
+      #elem{
+          
+          display: none;
+      }
+  }
+
+  footer a:hover {
+      text-decoration: underline;
+      text-underline-offset: 2px;
+
+  }
+
+  #separator {
+      padding-top: 1px;
+      background-color: #1f3344;
+  }
+
+  .widget {
+      margin-bottom: 0px;
+  }
+
+  .footer .widget .widget-title {
+      margin-top: 0px;
+  }
+</style>
+
+<!-- end wrapper -->
+
+<!-- Footer Scripts -->
+<!-- JS | Chart-->
+<script src="{{ asset('app-assets/js/chart.js') }}" defer></script>
+<!-- JS | Custom script for all pages -->
+<script src="{{ asset('app-assets/js/custom.js') }}" defer></script>
+
+<!-- SLIDER REVOLUTION 5.0 EXTENSIONS
+    (Load Extensions only on Local File Systems !
+     The following part can be removed on Server for On Demand Loading) -->
+<script src="{{ asset('app-assets/js/revolution-slider/js/extensions/revolution.extension.actions.min.js') }}" defer>
+</script>
+<script src="{{ asset('app-assets/js/revolution-slider/js/extensions/revolution.extension.carousel.min.js') }}" defer>
+</script>
+<script src="{{ asset('app-assets/js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js') }}" defer>
+</script>
+<script src="{{ asset('app-assets/js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js') }}"
+  defer></script>
+<script src="{{ asset('app-assets/js/revolution-slider/js/extensions/revolution.extension.migration.min.js') }}" defer>
+</script>
+<script src="{{ asset('app-assets/js/revolution-slider/js/extensions/revolution.extension.navigation.min.js') }}"
+  defer></script>
+<script src="{{ asset('app-assets/js/revolution-slider/js/extensions/revolution.extension.parallax.min.js') }}" defer>
+</script>
+<script src="{{ asset('app-assets/js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js') }}"
+  defer></script>
+<script src="{{ asset('app-assets/js/revolution-slider/js/extensions/revolution.extension.video.min.js') }}" defer>
+</script>
+
+</body>
+
+</html>
+
